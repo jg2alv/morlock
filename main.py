2d0a704ff7a790e4029a818021b27a6df94b0b3e
João Vaz <jg2alv@gmail.com>
2022-11-27 21:52:48 -0300

    Remove argparse redundancy

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import argparse
 from Morlock import Morlock
 
-def parse_args():
-    parser = argparse.ArgumentParser(prog='morlock')
-    parser.add_argument('file', nargs='?', default=None, type=str)
-    parser.add_argument('-p', '--password', required=False, default=None, type=str)
-
-    return parser.parse_args()
-
 def main():
-    args = parse_args()
-    morlock = Morlock()
-
-    morlock.set_file(args.file)
-    morlock.set_password(args.password)
-    morlock.cmdloop()
+    Morlock().cmdloop()
 
 if __name__ == '__main__':
     main()
